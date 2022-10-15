d2b0907479545ad48ed7b9c70a2232fdb2a4b479
Paula <paulactalim@gmail.com>
2022-10-14 22:32:12 -0300

    traducao parcial do codigo

--- pilha/pilha.c
+++ pilha/pilha.c
@@ -11,66 +11,46 @@ void Pilha (struct Pilha pilha) {
 }
 
 //Metodos setting
-    void setN (int n) {
-        // if (n >= array.length) {
-        //     throw new Exception ("Erro");
-        // }
-        
-        this->n = n;
+void setN (int n) {    
+    n = n;
+}
+
+void setArray (int pos, int num) {
+    array[pos] = num;
+}
+
+//Metodo getting
+int getN () {
+    return n;
+}
+
+int getArrayElemento (int pos) {
+    return array[pos];		
+}
+
+void inserir (int num) {
+    array[n] = num;
+    n++;
+}
+
+int remover () {
+    n--;
+    return array[n];
+}
+
+/*
+* Descricao: essa funcao exibe os numeros de um vetor de inteiros
+* Parametro: um vetor de inteiros (vetor a ser exibido)
+*/
+void exibir () {
+    printf ( "[ ");
+
+    if (n != 0) {
+        for (int i = 0; i < n-1; i++) {
+            printf( "%d, ", array[i]);
+        }
+        printf ("%d", array[n-1]);
     }
 
-    void setArray (int pos, int num) {
-        // if (pos < 0 || pos >= n) {
-        //     throw new Exception ("Erro");
-        // }
-		
-		this->array[pos] = num;
-	}
-	
-	//Metodo getting
-	int getN () {
-		return this->n;
-	}
-
-	int getArrayElemento (int pos) {
-        // if (pos < 0 || pos >= n) {
-        //     throw new Exception ("Erro");
-        // }
-
-		return this->array[pos];		
-	}
-    
-    void inserir (int num) {
-		// if (n >= array.length) {
-		// 	throw new Exception ("Erro");
-		// }
-		
-        this->array[this->n] = num;
-        this->n++;
-    }
-
-    int remover () {
-		// if (this.n == 0) {
-		// 	throw new Exception ("Erro");
-		// }
-
-        n--;
-        return array[n];
-    }
-
-    /*
-    * Descricao: essa funcao exibe os numeros de um vetor de inteiros
-    * Parametro: um vetor de inteiros (vetor a ser exibido)
-    */
-	void exibir () {
-		cout << "[ ";
-
-		if (n != 0) {
-			for (int i = 0; i < n-1; i++) {
-				cout << array[i] << ", ";
-			}
-			cout << array[n-1];
-		}
-
-		cout << " ]" << endl;
-	}
+    printf ( " ]\n");
+}
